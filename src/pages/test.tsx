e301d56d1e805558b429479b028f9a9547a34939
toasahi <asa1313to@gmail.com>
2022-07-26 18:18:31 +0900

    first commit

---
+++ src/pages/test.tsx
@@ -0,0 +1,13 @@
+import useAspidaSWR from "@aspida/swr";
+import {apiClient} from "~/utils/apiClient";
+
+const Test = () => {
+    const { data: data} = useAspidaSWR(apiClient.test)
+    return(
+        <div>
+            <h1>{data}</h1>
+        </div>
+    )
+}
+
+export default Test
---
+++ src/utils/$path.ts
@@ -0,0 +1,24 @@
+import type { OptionalQuery as OptionalQuery0 } from '../pages/article'
+import type { Query as Query1 } from '../pages/article/entry'
+
+export const pagesPath = {
+  "article": {
+    "entry": {
+      $url: (url: { query: Query1, hash?: string }) => ({ pathname: '/article/entry' as const, query: url.query, hash: url.hash })
+    },
+    $url: (url?: { query?: OptionalQuery0, hash?: string }) => ({ pathname: '/article' as const, query: url?.query, hash: url?.hash })
+  },
+  "test": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/test' as const, hash: url?.hash })
+  },
+  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
+}
+
+export type PagesPath = typeof pagesPath
+
+export const staticPath = {
+  favicon_png: '/favicon.png',
+  vercel_svg: '/vercel.svg'
+} as const
+
+export type StaticPath = typeof staticPath
